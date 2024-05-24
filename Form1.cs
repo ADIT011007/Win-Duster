63cf49496dfab1685488dd863b38fb4ffeb0b6ba
Adit Gadkary <Adit.Gadkary@outlook.com>
2024-05-24 11:09:59 +0530

    1

--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MetroFramework.Controls;
+using System;
 using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Win_Duster
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroFramework.Forms.MetroForm
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        
     }
 }
+
